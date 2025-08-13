10538a89f5f694bfb08eb23af50daf8228dd050d
Saif Jawed <saifjavedranchi@gmail.com>
2025-08-13 21:16:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://skillnest-eta.vercel.app"],
+    origin: true,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], //include Authorization here
   })
