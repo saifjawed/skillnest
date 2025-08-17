0d1926683959c786020f5034aa4e75c1012d9fd7
Saif Jawed <saifjavedranchi@gmail.com>
2025-08-17 13:21:21 +0530

    Update apiconnector.js

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  withCredentials: true,   // ✅ ensures cookies (token) are saved and sent
+});
 
 export const apiConnector = (method, url, bodyData = null, headers = {}, params = null) => {
-    console.log("API CALL →", method, url);
-console.log("→ Headers:", headers);
+  console.log("API CALL →", method, url);
+  console.log("→ Headers:", headers);
 
   return axiosInstance({
     method,
